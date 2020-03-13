d675c3d128e45c42349f0d53d7511a418135af25
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2020-03-13 12:16:10 -0500

    finished ht problem 2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -20,4 +20,33 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
 
-    pass
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
+
+    route[0] = hash_table_retrieve(hashtable, 'NONE')
+
+    for i in range(0, len(route)):
+        if hash_table_retrieve(hashtable, route[i]) == "NONE":
+            break
+        else:
+            route[i + 1] = hash_table_retrieve(hashtable, route[i])
+        
+
+    return route[:length - 1]
+
+
+ticket_1 = Ticket("PIT", "ORD")
+ticket_2 = Ticket("XNA", "SAP")
+ticket_3 = Ticket("SFO", "BHM")
+ticket_4 = Ticket("FLG", "XNA")
+ticket_5 = Ticket("NONE", "LAX")
+ticket_6 = Ticket("LAX", "SFO")
+ticket_7 = Ticket("SAP", "SLC")
+ticket_8 = Ticket("ORD", "NONE")
+ticket_9 = Ticket("SLC", "PIT")
+ticket_10 = Ticket("BHM", "FLG")
+
+tickets = [ticket_1, ticket_2, ticket_3, ticket_4, ticket_5,
+            ticket_6, ticket_7, ticket_8, ticket_9, ticket_10]
+
+print(reconstruct_trip(tickets, 10))
